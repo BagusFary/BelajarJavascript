a30f4a5d99a6b26e7a460d59e728c3d235da23d7
BagusFary <bagusfaryananta@gmail.com>
2023-07-06 20:53:23 +0700

    Looping

--- Looping/main.js
+++ Looping/main.js
@@ -1,13 +1,37 @@
 
 
 const x = [3,5,12];
-let total = 0
+let totalFor = 0
 
 for(let i = 0; i < x.length; i++){
-    console.log(`${x}`)
+    totalFor = totalFor + x[i]
+    console.log(x[i])
 }
 
-while() {
-    
+
+console.log(`Total menggunakan For = ${totalFor}`)
+
+let i = 0
+let totalWhile = 0
+
+
+while(i < x.length){
+    console.log(x[i])
+    totalWhile = totalWhile + x[i]
+    i++
 }
 
+
+console.log(`Total menggunakan While = ${totalWhile}`)
+
+let totalDoWhile = 0
+
+do{
+    console.log(x[i])
+    totalDoWhile = totalDoWhile + x[i]
+    i++
+}
+
+while(i < x.length)
+
+console.log(`Total menggunakan Do While ${totalDoWhile}`)
